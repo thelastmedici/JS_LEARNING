40a496c022d921f4af6ff3eeddde5e6a5bd895ad
thelastmedici <christianopeyemijoshua@gmail.com>
2025-03-08 15:57:11 +0100

    added some javascript function

--- ping-coming-soon-page/index.js
+++ ping-coming-soon-page/index.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.notifyForm');
-    form.addEventListener('click', function(e) {
+    const mail = document.getElementById('email');
+    const errorMsg = document.getElementById('emailError');
+    const notifyBtn = document.querySelector('.notifyBtn');
+    notifyBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        
         console.log(" add email");
     });
 } );
