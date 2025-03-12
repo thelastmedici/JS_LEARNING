354e21ea4592720f13aaa377dbe4cff8c527ec40
thelastmedici <christianopeyemijoshua@gmail.com>
2025-03-12 09:37:11 +0100

    added reset functionality for input field

--- ping-coming-soon-page/index.js
+++ ping-coming-soon-page/index.js
@@ -2,26 +2,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const email = document.getElementById('email');
     const errorMsg = document.getElementById('emailError');
     const notifyBtn = document.querySelector('.notifyBtn');
-
+    const inputField = document.getElementById('email');
+   
     notifyBtn.addEventListener('click', function(e) {
         e.preventDefault();
-        let isValid = true;
         const emailPattern = /^[^\s@]+@[^\s@]+\.[a-z]{2,}$/;
         
         if (!email.value.match(emailPattern)) {
             errorMsg.textContent = "Please provide a valid email address";
-            errorMsg,style.display = "block";
+            errorMsg.style.display = "block";
             email.style.border = "2px solid red";
         
         } else {
             errorMsg.textContent = "";
             errorMsg.style.display = "none";
             email.style.border = "2px solid hsl(0, 0%, 90%)";
-        }
-        if (isValid){
-            // Send email notification here
             alert("Email notification sent successfully");
             email.value = '';
+        
         }
+            
+    });
+
+    inputField.addEventListener('focus', function() {
+        errorMsg.textContent = "";
+        errorMsg.style.display = "none";
+        email.style.border = "2px solid hsl(0, 0%, 90%)";
     });
 });
