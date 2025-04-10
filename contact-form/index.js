bc6b280b01c809ab446246ad16b2cfe48a731e69
thelastmedici <christianopeyemijoshua@gmail.com>
2025-04-10 20:07:42 +0100

    added styles

---
+++ contact-form/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const fName = document.querySelector('.F-name');
+  const lName = document.querySelector('.L-name');
+  const emailInput = document.querySelector('input[type="email"]');
+  const radio1 = document.querySelector('.radio1');
+  const radio2 = document.querySelector('.radio2');
+  
+
+   const submitBtn = document.querySelector('button');
+   submitBtn.addEventListener('click', function(){
+   alert("work");
+  
+
+  });
+
+   
+
+});
