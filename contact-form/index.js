069912589f4d5c8c1e66e2ea6e7c3d3e5ba818ca
thelastmedici <christianopeyemijoshua@gmail.com>
2025-04-10 21:50:36 +0100

    added variables to index.js

--- contact-form/index.js
+++ contact-form/index.js
@@ -4,16 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
   const emailInput = document.querySelector('input[type="email"]');
   const radio1 = document.querySelector('.radio1');
   const radio2 = document.querySelector('.radio2');
-
+  const messageTextarea = document.querySelector('.message textarea');
+  const consentChheck = document.querySelector('input[type="checkbox"]');
   const fNameError = document.getElementById("#fnameError");
   const lNameError = document.getElementById("#lnameError");
   const emailError = document.getElementById("#emailError");
+  const msgError = document.getElementById('#msgError')
   
 
    const submitBtn = document.querySelector('button');
    submitBtn.addEventListener('click', function(e){
    e.preventDefault();
-   if(fName.value.trim() === "")
+   [fNameError,lNameError,emailError,msgError]
+   
   
 
   });
