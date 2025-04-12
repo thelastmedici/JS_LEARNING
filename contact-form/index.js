13a2c2c1630a6f889ccdc8dc5d8c91b214383793
thelastmedici <christianopeyemijoshua@gmail.com>
2025-04-12 21:58:33 +0100

    made adjustment

--- contact-form/index.js
+++ contact-form/index.js
@@ -2,11 +2,30 @@ document.addEventListener('DOMContentLoaded', function() {
   //create helper function
   const $ = (selector) =>document.querySelector(selector);
   
-  const fields ={
+  const fields = {
     fName:{
       input: $('.F-name input'),
       error:$('#fnameError'),
       container:$('.F-name')
+    },
+
+    lName:{
+      input:$('.L-name input'),
+      error:$('#lnameError'),
+      container:$('.L-name'),
+    },
+    
+    email:{
+      input:$('input[type="email"]'),
+      error:$('#emialError'),
+    },
+    message:{
+      input:$('.message textarea'),
+      error:$('#msgError'),
+    },
+    consent:{
+      input:$('input[type="checkbox"]'),
+      error:$('#consentError'),
     }
   }
 
