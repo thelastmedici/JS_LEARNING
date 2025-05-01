750a177770f9993c4abc59cc972c0efe81a35bac
thelastmedici <christianopeyemijoshua@gmail.com>
2025-05-01 21:02:37 +0100

    added markup

---
+++ fylo-data-storage-component-master/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          raleway: ['Raleway', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
