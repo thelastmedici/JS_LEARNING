ee6c7f0794996d445f5edd2d1f3d151e79179fdd
thelastmedici <christianopeyemijoshua@gmail.com>
2025-05-26 10:46:27 +0100

    added screens

--- fylo-data-storage-component-master/tailwind.config.js
+++ fylo-data-storage-component-master/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
           raleway: ['Raleway', 'sans-serif'],
         },
       },
+      
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
     },
     plugins: [],
   };
